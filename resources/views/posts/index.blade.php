ce4acfd1f3f1a3a0d6546fb2000670d0b6639009
mkhotamirais <mkhotamirais@gmail.com>
2024-11-27 08:54:54 +0700

    update lagi

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 
-    <h1 class="title">Latest Postsss</h1>
+    <h1 class="title">Latest Posts</h1>
 
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
         @foreach ($posts as $post)
